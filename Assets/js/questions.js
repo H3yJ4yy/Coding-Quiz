20f2dea91f2095ae06cc420efd8f7018e50e9682
Andrea Gonzalez <H3y.J4y@Araths-iMac.lan>
2023-10-03 16:50:56 -0500

    init

---
+++ Assets/js/questions.js
@@ -0,0 +1,128 @@
+// questions 
+const questions = [
+    {
+        question: "Javascript is an ___ language.",
+        choices: ["a. object-oriented", "b. object-based", "c. procedural", "d. none of the above"],
+        correctAnswer: "a. object-oriented"
+    },
+    {
+        question: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["a. var", "b. let", "c. both a and b ", "d. none of the above"],
+        correctAnswer: "c. both a and b"
+    },
+
+    {
+        question: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["a. getElementbyId()", "b. getElementbyClassName()", "c. both a and b", "none of the above"],
+        correctAnswer: "c. both a and b"
+    },
+
+    {
+        question: "Upon encountering empty statements, what does the Javascript Interpreter do?",
+        choices: ["a. throws an error", "b. ignores the statements", "c. gives a warning", "d. none of the above"],
+        correctAnswer: "b. ignores the statements"
+    },
+
+    {
+        question: "Which of the following methods can be used to display data in some form using Javascript?",
+        choices: ["a. document.write()", "b. console.log()", "c. window.alert()", "d. all of the above"],
+        correctAnswer: "d. all of the above"
+    },
+
+    {
+        question: "When the switch statement matches the expression with the given labels, how is the comparison done?",
+        choices: ["a. both the datatype and the result of the expression are compared", "b. only the datatype of the expresson is compared", "c. only the value of the expression is compared", "d. none of the above"],
+        correctAnswer: "a. both the datatype and the result of the expression are compared"
+    },
+   
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        choices: ["a. in", "b. is in", "c. exist", "d. lies"],
+        correctAnswer: "a. in"
+    },
+   
+    {
+        question: "What does the Javascript “debugger” statement do?",
+        choices: ["a. it will debug all the errors in the program at runtime", "b. it acts as a breakpont in a program", "c. it will debug error in the current statement if any", "d. all of the above"],
+        correctAnswer: "b. it acts as a breakpont in a program"
+    },
+   
+    {
+        question: "What does the 'toLocateString()' method do in JS?",
+        choices: ["a. returns a localised obejct representation", "b. returns a parsed string", "c. returns a localised string represantaion of an object", "d. none of the above"],
+        correctAnswer: "c. returns a localised string represantaion of an object"
+    },
+   
+    {
+        question: "The process in which an object or data structure is translated into a format suitable for transferral over a network, or storage is called?",
+        choices: ["a. object serialization", "b. object encapsulation", "c. object inheritance", "d. none of the above"],
+        correctAnswer: "a. object serialization"
+    },
+   
+    {
+        question: "What is the output of the following code snippet: print(NaN === NaN);",
+        choices: ["a. true", "b. false", "c. undefined", "d. error"],
+        correctAnswer: "b. false"
+    },
+   
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        choices: ["a. boolean", "b. undefined", "c. object", "d. integer"],
+        correctAnswer: "c. object"
+    },
+   
+    {
+        question: "Which function is used to serialize an object into a JSON string in Javascript?",
+        choices: ["a. stringify()", "b. parse()", "c. convert()", "d. none of the above"],
+        correctAnswer: "a. stringify()"
+    },
+   
+    {
+        question: "The 3 basic object attributes in Javascript are:",
+        choices: ["a. class, prototype, and objects parameters", "b. class, prototype, and objects extensible flag", "c. class, parameters, and objects extensible flag", "d. classes, native object, and interprises"],
+        correctAnswer: "b. class, prototype, and objects extensible flag"
+    },
+   
+    {
+        question: "Which of the following are closures in Javascript?",
+        choices: ["a. variables", "b. functions", "c. objects", "d. all of the above"],
+        correctAnswer: "d. all of the above"
+    },
+   
+
+    {
+        question: "Which of the following is not a Javascript framework?",
+        choices: ["a. node", "b. vue", "c. react", "d. cassandra"],
+        correctAnswer: "d. cassandra"
+    },
+   
+
+    {
+        question: "What keyword is used to declare an asynchronous function in Javascript?",
+        choices: ["a. async", "b. await", "c. setTimeout", "d. none of the above"],
+        correctAnswer: "a. async"
+    },
+   
+
+    {
+        question: "How to stop an interval timer in Javascript?",
+        choices: ["a. clearInterval", "b. clearTimer", "c. intervalOver", "d. none of the above"],
+        correctAnswer: "a. clearInterval"
+    },
+   
+
+    {
+        question: "What does … operator do in JS?",
+        choices: ["a. it is used to spread iterables to individual elements", "b. it is used to describe a datatype of undefined size", "c. no such operator exists", "d. none of the above"],
+        correctAnswer: "a. it is used to spread iterables to individual elements"
+    },
+   
+
+    {
+        question: "How do we write a comment in javascript?",
+        choices: ["a. /**/", "b. //", "c. #", "d. $$"],
+        correctAnswer: "b. //"
+    },
+   
+
+];
